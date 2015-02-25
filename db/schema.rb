f5b7babedd9b52dd5f9044344b261ea44294dd80
saeedsimin <sr.shehnepoor@gmail.com>
2015-02-25 14:39:20 -0800

    last version

--- db/schema.rb
+++ db/schema.rb
@@ -27,11 +27,4 @@ ActiveRecord::Schema.define(version: 20150218094449) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
